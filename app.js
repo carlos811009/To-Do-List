a387bf228ce3be42779d3eea468a09e2e4ef71d1
carlos <carlos811009@gmail.com>
2021-06-20 22:40:16 +0800

    add login & register page

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
 app.listen(PORT, () => {
   console.log(`the web is runnung on http://localhost:${PORT}`)
 })
