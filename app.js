f93247386c8c9beb21410e9d401b9d3bd2aa3be7
carlos <carlos811009@gmail.com>
2021-06-20 22:54:20 +0800

    create User table

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-
+const db = require('./models/index')
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
